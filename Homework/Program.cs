bd190142861359094f4e5d1bd99470f133fd71c2
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-11-04 16:22:30 +0200

    Fixed

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -8,67 +8,6 @@ namespace Homework
     {
         public static void Main(string[] args)
         {
-            // #region task strings
-
-            //    Console.WriteLine("Enter the string:");
-            //    string text_word = Console.ReadLine();
-
-            //    string[] word = text_word.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            //    for (int i = 0; i < word.Length; i++)
-            //    {
-            //        foreach (var item in word[i])
-            //        {
-            //            if (char.IsDigit(item))
-            //            {
-            //                word[i] = word[i].Replace(word[i], " ");
-            //            }
-            //        }
-
-            //        if (i % 2 == 0)
-            //        {
-            //            word[i] = new string(word[i].Reverse().ToArray());
-            //        }
-            //    }
-
-            //    for (int k = 0; k < word.Length; k++)
-            //    {
-            //        if (word[k].Length > 0)
-            //        {
-            //            word[k] = word[k].Substring(0, 1).ToUpper() + word[k].Substring(1, word[k].Length - 1).ToLower();
-            //        }
-            //    }
-
-            //    Console.WriteLine("Result:");
-            //    for (int j = 0; j < word.Length; j++)
-            //    {
-            //        Console.Write(word[j] + " ");
-            //    }
-
-            //    Console.WriteLine();
-            //    Console.WriteLine("Task 3:");
-            //    string[] stroka = { "Port", "green", "placen" };
-
-            //    for (int d = 0; d < stroka.Length; d++)
-            //    {
-            //        if (stroka[d].StartsWith("p", true, new System.Globalization.CultureInfo("en-US")))
-            //        {
-            //            stroka[d] = stroka[d].Replace("p", "S", true, new System.Globalization.CultureInfo("en-US"));
-            //        }
-
-            //        if (stroka[d].EndsWith("n", true, new System.Globalization.CultureInfo("en-US")))
-            //        {
-            //            stroka[d] = stroka[d].Replace("n", "O", true, new System.Globalization.CultureInfo("en-US"));
-            //        }
-            //    }
-
-            //    for (int u = 0; u < stroka.Length; u++)
-            //    {
-            //        Console.WriteLine(stroka[u]);
-            //    }
-            //  #endregion
-            #region task array
-
             Random random = new Random();
             Console.WriteLine("Enter count of element to fill array:");
             int array_len = int.Parse(Console.ReadLine());
@@ -182,8 +121,6 @@ namespace Homework
             Console.WriteLine();
             Console.WriteLine(count > count_two ? "First has more upper register chars" :
                 "Second has more upper register chars");
-
-            #endregion
         }
     }
 }
